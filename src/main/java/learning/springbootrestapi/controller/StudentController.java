7e45beb5752b6d67f0c2ee4f2f284b866bf2b5f4
drew-worden <drew.p.worden@gmail.com>
2023-04-18 11:27:31 -0400

    Added a base URL with Request Mapping annotation.

--- src/main/java/learning/springbootrestapi/controller/StudentController.java
+++ src/main/java/learning/springbootrestapi/controller/StudentController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 //STUDENT CONTROLLER
 @RestController
+@RequestMapping("students")
 public class StudentController {
     //GET ENDPOINT FOR RETURNING STATIC SINGLE JSON OBJECT
     @GetMapping("student")
@@ -23,7 +24,7 @@ public class StudentController {
     }
 
     //GET ENDPOINT FOR RETURNING STATIC MULTIPLE JSON OBJECT
-    @GetMapping("students")
+    @GetMapping
     public List<Student> getStudents() {
         List<Student> students = new ArrayList<>();
         students.add(new Student(1, "Drew", "Worden"));
@@ -33,19 +34,19 @@ public class StudentController {
     }
 
     //GET ENDPOINT FOR PATH VARIABLE
-    @GetMapping("student/{id}/{firstName}/{lastName}")
+    @GetMapping("{id}/{firstName}/{lastName}")
     public Student studentPathVariable(@PathVariable("id") int studentId, @PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName) {
         return new Student(studentId, firstName, lastName);
     }
 
     //GET ENDPOINT FOR QUERY PARAMETER
-    @GetMapping("student/query")
+    @GetMapping("query")
     public Student studentQueryParameter(@RequestParam int id, @RequestParam String firstName, @RequestParam String lastName) {
         return new Student(id, firstName, lastName);
     }
 
     //POST ENDPOINT FOR CREATING A NEW STUDENT
-    @PostMapping("student/create")
+    @PostMapping("create")
     @ResponseStatus(HttpStatus.CREATED)
     public Student createStudent(@RequestBody Student student) {
         System.out.println(student.getId());
@@ -55,7 +56,7 @@ public class StudentController {
     }
 
     //PUT ENDPOINT FOR UPDATING A STUDENT
-    @PutMapping("student/update/{id}")
+    @PutMapping("update/{id}")
     public Student updateStudent(@RequestBody Student student, @PathVariable int id) {
         System.out.println(student.getFirstName());
         System.out.println(student.getLastName());
@@ -63,7 +64,7 @@ public class StudentController {
     }
 
     //DELETE ENDPOINT FOR DELETING A STUDENT
-    @DeleteMapping("student/delete/{id}")
+    @DeleteMapping("delete/{id}")
     public String deleteStudent(@PathVariable int id) {
         System.out.println(id);
         return "Student deleted successfully.";
