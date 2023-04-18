e62f493c3b3dbb38df6ff9e2045095235ef68701
drew-worden <drew.p.worden@gmail.com>
2023-04-18 10:15:17 -0400

    Added students endpoint for returning multiple students.

--- src/main/java/learning/springbootrestapi/controller/StudentController.java
+++ src/main/java/learning/springbootrestapi/controller/StudentController.java
@@ -4,8 +4,13 @@ import learning.springbootrestapi.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
+    //GET ENDPOINT FOR RETURNING STATIC SINGLE JSON OBJECT
     @GetMapping("student")
     public Student getStudent() {
         Student student = new Student(
@@ -14,5 +19,15 @@ public class StudentController {
                 "Worden"
         );
         return student;
+    }
+
+    //GET ENDPOING FOR RETURNING STATIC MULTIPLE JSON OBJECT
+    @GetMapping("students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Drew", "Worden"));
+        students.add(new Student(2, "John", "Smith"));
+        students.add(new Student(3, "Jane", "Smith"));
+        return students;
     }
 }
